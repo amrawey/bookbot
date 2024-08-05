f7129de74e4b5d7ff984f2b8dd09accd38f2e97d
amrawey <amrkhalifa42@gmail.com>
2024-08-05 07:56:23 +0300

    Added Main.py and edited it

---
+++ main.py
@@ -0,0 +1,11 @@
+def main() :
+    book_path = 'books/frankenstein.txt'
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(book_path)
+    
+    with open(book_path) as f:
+        file_contents = f.read()
+
+main()
